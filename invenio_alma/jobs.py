9b9982f507f6d46057709c30204b962cb9333537
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-06-10 09:52:37 +0200

    fix(jobs): add customization of workflow

--- invenio_alma/jobs.py
+++ invenio_alma/jobs.py
@@ -7,9 +7,21 @@
 
 """Jobs."""
 
-from invenio_jobs.jobs import JobType
+from invenio_jobs.jobs import JobType, PredefinedArgsSchema
+from invenio_jobs.models import Job
+from marshmallow.fields import String
 
 from .tasks import create_alma_records, update_repository_records
+
+
+class AlmaPredefinedArgsSchema(PredefinedArgsSchema):
+    """Alma predefined args schema."""
+
+    workflow = String(
+        metadata={
+            "description": "choose workflow to run the task",
+        },
+    )
 
 
 class CreateAlmaRecordsJob(JobType):
@@ -21,6 +33,18 @@ class CreateAlmaRecordsJob(JobType):
 
     task = create_alma_records
 
+    arguments_schema = AlmaPredefinedArgsSchema
+
+    @classmethod
+    def build_task_arguments(
+        cls,
+        job_obj: Job,  # noqa: ARG003
+        workflow: str | None = None,
+        **__: dict,
+    ) -> dict[str, str]:
+        """Define extra arguments to be injected on task execution."""
+        return {"workflow": workflow}
+
 
 class UpdateRepositoryRecordsJob(JobType):
     """Update repository records job."""
@@ -30,3 +54,15 @@ class UpdateRepositoryRecordsJob(JobType):
     description = "Update record in the repository."
 
     task = update_repository_records
+
+    arguments_schema = AlmaPredefinedArgsSchema
+
+    @classmethod
+    def build_task_arguments(
+        cls,
+        job_obj: Job,  # noqa: ARG003
+        workflow: str | None = None,
+        **__: dict,
+    ) -> dict[str, str]:
+        """Define extra arguments to be injected on task execution."""
+        return {"workflow": workflow}
